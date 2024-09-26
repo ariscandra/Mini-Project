cff894b92f19a36e748435d36c779e65ddd97e3b
ariscandra <129823823+ariscandra@users.noreply.github.com>
2024-09-26 13:08:55 +0800

    Add files via upload

---
+++ praktikum_input.py
@@ -0,0 +1,15 @@
+# # jari-jari
+# jari = 5
+
+# # pi
+# pi = 3.14
+
+# # hitung luas
+# luas = pi * (jari ** 2)
+
+# # hasil
+# print(luas)
+pi = 3.14
+jari = int(input("Masukkan Jari-Jari dari lingkaran: "))
+hasil = pi * jari * jari
+print(hasil)
